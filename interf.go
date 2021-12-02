c791c6a49d5d91d48f63a073da0d8f71112d3877
Licoy <licoycn@gmail.com>
2021-12-02 13:01:59 +0800

    :art: 改进结构

--- interf.go
+++
@@ -1,6 +0,0 @@
-package stail
-
-type STail interface {
-	Tail(filepath string, tailLine int, call func(content string)) (err error)
-	Total(filepath string, call func(content string)) (err error)
-}
--- stail.go
+++ stail.go
@@ -10,6 +10,11 @@ import (
 	"strconv"
 )
 
+type STail interface {
+	Tail(filepath string, tailLine int, call func(content string)) (err error)
+	Total(filepath string, call func(content string)) (err error)
+}
+
 type sTail struct {
 	os  string
 	opt Options
